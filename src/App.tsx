766115f3b94ed7b94b69f1ca84050856c505fc94
Ming Chi <wong_mingchi@yahoo.co.uk>
2025-03-26 23:45:06 +0000

    components and folder structure implemented:

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import Homepage from './pages/homepage/Homepage'
+import Reviews from './pages/reviews/Reviews'
+import Category from './pages/category/Category'
+import Contact from './pages/contact/Contact'
 
 function App() {
-  
-
   return (
     <div>
-      <Homepage/>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/reviews" element={<Reviews/>}/>
+        <Route path='/category' element={<Category/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        
+      </Routes>
     </div>
   )
 }
